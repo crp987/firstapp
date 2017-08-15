6dcc2dbbf059ef9cc7b83ea8d3069d51618d2a0d
Chetan Prajapati <crprajapati111@gmail.com>
2017-08-15 20:49:21 +0530

    initialcommit

---
+++ cabinet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cabinet::Application.config.secret_token = '71590b34c96f34fcbe922e65760ea150e55f51f638407401815b7df41939216cf9e211e39ba65dc7fbce2de0db033588f6b4ee0361c5da1e4584410976fceb38'
